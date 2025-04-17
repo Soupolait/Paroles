c74da0ff275f7d4887b3805ac25cf0e8bda76b7a
Soupolait <loicperrot17000@pm.me>
2025-04-17 17:23:44 +0200

    Working filepicker, not gonna lie, AI helped me more than an hour long tutorial from a very nice India guy.

--- GUI/views/parameters.py
+++ GUI/views/parameters.py
@@ -1,10 +1,27 @@
 import flet as ft
 from flet_route import Params, Basket
-from flet import (Page, FilePicker, Text,
-                  ElevatedButton, Row, Column, FilePickerResultEvent)
+from flet import FilePicker, FilePickerResultEvent
 import subprocess
 
+selected_folder_path = None
+
 def parameters(page: ft.Page, params: Params, basket: Basket):
+    global selected_folder_path
+
+    folder_text = ft.Text(value="Aucun dossier sélectionné.")
+
+    file_picker = FilePicker(
+        on_result=lambda e: handle_folder_selected(e, folder_text)
+    )
+    page.overlay.append(file_picker)
+
+    def handle_folder_selected(e: FilePickerResultEvent, text_element: ft.Text):
+        global selected_folder_path
+        if e.path:
+            selected_folder_path = e.path
+            text_element.value = f"Dossier sélectionné : {selected_folder_path}"
+            page.update()
+
 
     return ft.View(
         "/parameters/",
@@ -26,8 +43,9 @@ def parameters(page: ft.Page, params: Params, basket: Basket):
                     controls=[
                         ft.ElevatedButton(
                             "Sélectionner un répertoire",
-                            on_click=lambda _: select_folder
+                            on_click=lambda _: file_picker.get_directory_path()
                         ),
+                        folder_text
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
                     horizontal_alignment=ft.CrossAxisAlignment.CENTER
