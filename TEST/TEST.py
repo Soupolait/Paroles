4149e76be41d0aff42f288474ce9cf51a31fadfb
Soupolait <loicperrot17000@pm.me>
2025-04-22 16:45:00 +0200

    Worked ?

--- TEST/TEST.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir =  os.path.dirname(current_dir)
-API_dir = os.path.abspath(os.chdir(root_dir/API))
-
-print(API_dir)
